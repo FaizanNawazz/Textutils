4008d724b21b61586fe72d5ad5042556f9555d9c
FaizanNawazz <thefaiziansari@gmail.com>
2024-12-30 00:24:21 +0500

    change

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -36,10 +36,20 @@ export default function TextForm(props) {
 <div className="container" style={{color : props.mode === 'light'? 'black' : 'white'}}>
     <h1>{props.heading}</h1>
   <textarea className={`form-control ${props.mode === 'light' ? 'light-mode' : 'dark-mode'}`} id="Mybox" value={text}  placeholder={"Type Here"} onChange={handleon} rows="8" style={{backgroundColor : props.mode === 'light'? 'white' :' rgba(33,37,41,1)', color : props.mode === 'light'? 'black' : 'white'}}></textarea>
-<button className="btn btn-primary my-3" onClick={upClick}>Convert To Uppercase</button>
-<button className="btn btn-primary my-3 mx-2" onClick={lowClick}>Convert To Lowercase</button>
-<button className="btn btn-primary my-3 " onClick={clearClick}>Clear Text</button>
-<button className="btn btn-primary my-3 mx-2" onClick={speak}>Speak</button>
+  <div className="d-flex flex-column flex-sm-row justify-content-start mt-3">
+    <button className="btn btn-primary mb-2 mb-sm-0 me-sm-2" onClick={upClick}>
+      Convert To Uppercase
+    </button>
+    <button className="btn btn-primary mb-2 mb-sm-0 me-sm-2" onClick={lowClick}>
+      Convert To Lowercase
+    </button>
+    <button className="btn btn-primary mb-2 mb-sm-0 me-sm-2" onClick={clearClick}>
+      Clear Text
+    </button>
+    <button className="btn btn-primary" onClick={speak}>
+      Speak
+    </button>
+  </div>
 </div>
 <div className="container my-2" style={{color : props.mode === 'light'? 'black' : 'white'}}>
   <h2>Your Text Summary</h2>
